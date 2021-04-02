1147057198dcab5ab843cb1aa20896f2a795566f
negismohit97 <78764199+negismohit97@users.noreply.github.com>
2021-04-01 22:10:50 -0400

    final Commit

---
+++ src/Business/Menu/MenuDirectory.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Menu;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jshar
+ */
+public class MenuDirectory {
+    private ArrayList<Menu> menuDirectory;
+    
+    public MenuDirectory() {
+        menuDirectory = new ArrayList();
+    }
+
+    public ArrayList<Menu> getMenuDirectory() {
+        return menuDirectory;
+    }
+
+    public void setMenuDirectory(ArrayList<Menu> menuDirectory) {
+        this.menuDirectory = menuDirectory;
+    }
+    
+    public Menu newItem(String item, double price, String restaurant) {
+        Menu menu = new Menu(item, price, restaurant);
+        menuDirectory.add(menu);
+        return menu;
+    }
+    
+    public void removeMenu(Menu menu) {
+        menuDirectory.remove(menu);
+    }
+}
